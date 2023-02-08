ed5a53d092364b172da8d0db37243fa3b2394dcf
Guilherme Dantas <guidanoli@hotmail.com>
2023-02-08 10:30:44 -0300

    fix: add diff outputs to `test/diff/`

---
+++ test/diff/forge-hardhat.lua
@@ -0,0 +1,28 @@
+return {
+  Difficulty = {
+    avg = {
+      absdiff = -39987,
+      reldiff = -0x1.7258dc7bd6891p-2,
+    },
+  },
+  Eligibility = {
+    avg = {
+      absdiff = -50587,
+      reldiff = -0x1.bf90f25dff60ep-3,
+    },
+  },
+  PoSV2Impl = {
+    avg = {
+      absdiff = -185455,
+      reldiff = -0x1.89ec2b88c2ddbp-4,
+    },
+    functions = {},
+  },
+  RewardManagerV2Impl = {
+    avg = {
+      absdiff = -85596,
+      reldiff = -0x1.0e13543764afdp-3,
+    },
+    functions = {},
+  },
+}
