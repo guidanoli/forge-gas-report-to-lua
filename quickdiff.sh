fee42d2b038d0ef169749a14dd86f39ef43a24b9
Guilherme Dantas <guidanoli@hotmail.com>
2023-02-28 17:36:52 +0700

    feat: add quickdiff.sh script

---
+++ quickdiff.sh
@@ -0,0 +1,27 @@
+#!/usr/bin/env bash
+
+# Quickly parse, diff and print gas reports
+# Usage: ./quickdiff.sh <a> <b>
+
+set -euo pipefail
+
+if [ $# -ge 2 ]
+then
+    apath=`readlink -f "$1"`
+    bpath=`readlink -f "$2"`
+    shift 2
+else
+    echo "Usage: $0 <a> <b>" >&2
+    exit 1
+fi
+
+cd "${BASH_SOURCE%/*}"
+
+a=`mktemp`
+b=`mktemp`
+
+lua main.lua parse --input "$apath" --output "$a"
+lua main.lua parse --input "$bpath" --output "$b"
+lua main.lua diff "$a" "$b" | lua main.lua printdiff
+
+rm -f "$a" "$b"
