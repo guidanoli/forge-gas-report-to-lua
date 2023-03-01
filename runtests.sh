c3ab33572e0a3145ba35d539138cf5f89214f665
Guilherme Dantas <guidanoli@hotmail.com>
2023-03-01 10:07:17 +0700

    fix: update run tests script
    * Let program detect format

--- runtests.sh
+++ runtests.sh
@@ -7,10 +7,10 @@ run() {
     lua main.lua "$@"
 }
 
-run parse --format forge --input test/input/forge1.txt --output test/output/forge1.lua
-run parse --format forge --input test/input/forge2.txt --output test/output/forge2.lua
-run parse --format hardhat --input test/input/hardhat1.txt --output test/output/hardhat1.lua
-run parse --format hardhat --input test/input/hardhat2.txt --output test/output/hardhat2.lua
+run parse --input test/input/forge1.txt --output test/output/forge1.lua
+run parse --input test/input/forge2.txt --output test/output/forge2.lua
+run parse --input test/input/hardhat1.txt --output test/output/hardhat1.lua
+run parse --input test/input/hardhat2.txt --output test/output/hardhat2.lua
 run diff test/output/forge1.lua test/output/forge2.lua --output test/diff/forge.lua
 run diff test/output/hardhat1.lua test/output/hardhat2.lua --output test/diff/hardhat.lua
 run printdiff --input test/diff/forge.lua --output test/diff/forge.md
